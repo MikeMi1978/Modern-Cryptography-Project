97e70ec42db38e7a924bf1b2cf1f7a5707b22996
Michael Mitchell-SETU <52199461+MikeMi1978@users.noreply.github.com>
2023-05-05 12:44:57 +0100

    Michael Mitchell Modern Cryptography Project Submission 05/05/23

--- api/handlers/auth.py
+++ api/handlers/auth.py
@@ -1,10 +1,17 @@
+# Modern Cryptography Project
+# Michael Mitchell Student # C00255151
 from datetime import datetime
 from time import mktime
 from tornado.gen import coroutine
+from cryptography.fernet import Fernet
 
 from .base import BaseHandler
 
 class AuthHandler(BaseHandler):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fernet = None
 
     @coroutine
     def prepare(self):
@@ -26,8 +33,13 @@ class AuthHandler(BaseHandler):
             'token': token
         }, {
             'email': 1,
-            'displayName': 1,
-            'expiresIn': 1
+            'fullName': 1,
+            'dateOfBirth': 1,
+            'phoneNumber': 1,
+            'disabilities': 1,
+            'address': 1,
+            'expiresIn': 1,
+            'encryptionKey': 1
         })
 
         if user is None:
@@ -40,8 +52,18 @@ class AuthHandler(BaseHandler):
             self.current_user = None
             self.send_error(403, message='Your token has expired!')
             return
-
+#Encryption key stored as part of the registration process is used to decrypt the data.
+        self.fernet = Fernet(user['encryptionKey'])
         self.current_user = {
             'email': user['email'],
-            'display_name': user['displayName']
+            'fullName': self.decrypt(user['fullName']),
+            'dateOfBirth': self.decrypt(user['dateOfBirth']),
+            'phoneNumber': self.decrypt(user['phoneNumber']),
+            'disabilities': self.decrypt(user['disabilities']),
+            'address': self.decrypt(user['address'])
         }
+
+    def decrypt(self, value):
+        if value is None:
+            return None
+        return self.fernet.decrypt(value).decode()
